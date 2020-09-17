22d075bcf49d82c782c8474c49b2012ca41a1a19
Baz <Baz>
2020-09-17 12:42:36 +0300

    fixed method printToConsole and printToConsoleTest also

--- homework_3/src/main/java/by/jrr/task/randomgenerator/RandomGeneratorUtil.java
+++ homework_3/src/main/java/by/jrr/task/randomgenerator/RandomGeneratorUtil.java
@@ -1,22 +1,19 @@
 package by.jrr.task.randomgenerator;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import java.util.Random;
 
 public class RandomGeneratorUtil {
-    public static final Logger LOGGER = LogManager.getLogger(RandomGeneratorUtil.class);
     private static final Random rnd = new Random();
 
-    private RandomGeneratorUtil(){}
+    private RandomGeneratorUtil() {
+    }
 
     public static int generateRandomNumber() {
 
         return rnd.nextInt(101);
     }
 
-    public static void printToConsole(int num) {
-        LOGGER.info(num);
+    public static String printToConsole(int num) {
+        return Integer.toString(num);
     }
 }
